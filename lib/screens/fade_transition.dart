cc2c14e07b3a5082672bbd938537c773072387da
Annsh Singh <annsh@so.city>
2019-03-31 11:25:04 +0530

    Updated fade transition widget

--- lib/screens/fade_transition.dart
+++ lib/screens/fade_transition.dart
@@ -1,8 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_guide/utils.dart';
 
-class FadeTransitionWidget extends StatelessWidget {
+class FadeTransitionWidget extends StatefulWidget {
+  @override
+  _FadeTransitionWidgetState createState() => _FadeTransitionWidgetState();
+}
+
+class _FadeTransitionWidgetState extends State<FadeTransitionWidget>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+  CurvedAnimation _curve;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 500),
+    );
+
+    _curve = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_curve);
+
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed)
+        _controller.reverse();
+      else if (status == AnimationStatus.dismissed) _controller.forward();
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          child: Center(
+            child: Text(
+              'Fade Transition Widget',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Utils.ubuntuRegularFont),
+            ),
+          ),
+          margin: EdgeInsets.only(right: 48),
+        ),
+      ),
+      body: Center(
+        child: FadeTransition(
+          opacity: _animation,
+          child: Icon(
+            Icons.favorite,
+            color: Colors.red,
+            size: 300,
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: Colors.lightBlue,
+        onPressed: () => setState(() {
+              _controller.forward();
+            }),
+        icon: Icon(
+          Icons.movie_filter,
+          color: Colors.white,
+        ),
+        label: Text(
+          "Animate",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
   }
 }
