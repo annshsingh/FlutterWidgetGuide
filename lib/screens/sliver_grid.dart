d2a83a898d25e56e66eab1bc89215ba4cea71413
Annsh Singh <annsh@so.city>
2019-04-05 15:11:23 +0530

    Updated Sliver Grid widget

--- lib/screens/sliver_grid.dart
+++ lib/screens/sliver_grid.dart
@@ -1,8 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_guide/utils.dart';
+import 'dart:math' as math;
 
 class SliverGridWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: <Widget>[
+          ///First sliver is the App Bar
+          SliverAppBar(
+            ///Properties of app bar
+            backgroundColor: Colors.white,
+            floating: false,
+            pinned: true,
+            expandedHeight: 200.0,
+
+            ///Properties of the App Bar when it is expanded
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text(
+                "SliverGrid Widget",
+                style: TextStyle(
+                    color: Colors.black87,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: Utils.ubuntuRegularFont),
+              ),
+              background: Container(
+                decoration: BoxDecoration(
+                  border: Border(
+                    top: BorderSide(
+                      color: Colors.black26,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 4,
+            ),
+            ///Lazy building of list
+            delegate: SliverChildBuilderDelegate(
+                  (BuildContext context, int index) {
+                /// To convert this infinite list to a list with "n" no of items,
+                /// uncomment the following line:
+                /// if (index > n) return null;
+                return listItem(Utils.getRandomColor(), "Sliver Grid item:\n$index");
+              },
+              /// Set childCount to limit no.of items
+              /// childCount: 100,
+            ),
+          )
+        ],
+      ),
+    );
   }
+
+  Widget listItem(Color color, String title) => Container(
+    height: 100.0,
+    color: color,
+    child: Center(
+      child: Text(
+        "$title",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+            color: Colors.white,
+            fontSize: 10.0,
+            fontWeight: FontWeight.bold,
+            fontFamily: Utils.ubuntuRegularFont),
+      ),
+    ),
+  );
 }
