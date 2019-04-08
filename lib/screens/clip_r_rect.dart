6d47e65e6e8a45911a8179a92a72cf5ac70d822a
Annsh Singh <annsh@so.city>
2019-04-08 11:11:14 +0530

    Updated ClipRRect widget

--- lib/screens/clip_r_rect.dart
+++ lib/screens/clip_r_rect.dart
@@ -1,8 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_guide/utils.dart';
 
-class ClipRRectWidget extends StatelessWidget {
+class ClipRRectWidget extends StatefulWidget {
+  @override
+  _ClipRRectWidgetState createState() => _ClipRRectWidgetState();
+}
+
+class _ClipRRectWidgetState extends State<ClipRRectWidget> {
+  var _value = 50.0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          child: Center(
+            child: Text(
+              'ClipRRect Widget',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Utils.ubuntuRegularFont),
+            ),
+          ),
+          margin: EdgeInsets.only(right: 48),
+        ),
+      ),
+      body: ListView(children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: 24, bottom: 24),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 200,
+                margin: EdgeInsets.all(12),
+                child: Stack(
+                  children: <Widget>[
+                    ClipRRect(
+                      child: Container(
+                        color: Colors.amber,
+                      ),
+                      borderRadius: BorderRadius.circular(_value),
+                      clipBehavior: Clip.antiAlias,
+                    ),
+                    Center(
+                      child: Container(
+                        margin: EdgeInsets.all(10),
+                        width: 100,
+                        height: 100,
+                        child: FlutterLogo(
+                          colors: Colors.lightBlue,
+                          textColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(12),
+                    child: Text(
+                      'Slide the slider widget to change the value of radius\n'
+                          'to observe changes on borders of the widget below',
+                      style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 14.0,
+                          fontStyle: FontStyle.italic,
+                          fontFamily: Utils.ubuntuRegularFont),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 24),
+                child: Slider(
+                  value: _value,
+                  activeColor: Colors.lightBlue,
+                  inactiveColor: Colors.lightBlue[50],
+                  min: 0.0,
+                  max: 100.0,
+                  divisions: 100,
+                  label: '${_value.round()}',
+                  onChanged: (double value) {
+                    setState(() {
+                      _value = value;
+                    });
+                  },
+                ),
+              ),
+              const Text('Drag to change value of Radius'),
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
