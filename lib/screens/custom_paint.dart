53de6c7db388624f3c0338ba4b95465654c389ed
Annsh Singh <annsh@so.city>
2019-04-10 12:31:35 +0530

    Updated Custom Paint widget

--- lib/screens/custom_paint.dart
+++ lib/screens/custom_paint.dart
@@ -1,8 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_guide/utils.dart';
 
-class CustomPaintWidget extends StatelessWidget {
+class CustomPaintWidget extends StatefulWidget {
+  @override
+  _CustomPaintWidgetState createState() => _CustomPaintWidgetState();
+}
+
+class _CustomPaintWidgetState extends State<CustomPaintWidget> {
+  var _bgColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            'CustomPaint Widget',
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: Utils.ubuntuRegularFont),
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.format_color_fill),
+              onPressed: () => setState(() {
+                    _bgColor == Colors.white
+                        ? _bgColor = Colors.black87
+                        : _bgColor = Colors.white;
+                  }),
+            ),
+          ]),
+      body: CustomPaint(
+        size: Size(MediaQuery.of(context).size.width,
+            MediaQuery.of(context).size.height),
+        painter: MyPainter(_bgColor),
+      ),
+    );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  MyPainter(this.color);
+
+  final Color color;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var rect = Offset.zero & size;
+
+    /// The .. syntax here is called a cascade.
+    /// With cascades, you can perform multiple operations
+    /// on the members of a single object
+    Paint rectPaint = Paint()..color = color;
+    canvas.drawRect(
+      rect,
+      rectPaint,
+    );
+
+    var width = size.width;
+    var height = size.height;
+
+    ///canvas.drawLine(Offset(to X, to Y), Offset(from X, from Y), linePaint);
+
+    ///tilt no1
+    canvas.drawLine(Offset(width / 2.5, height / 6),
+        Offset(width / 20, height / 2), _paint(Colors.amber));
+
+    ///top most line
+    canvas.drawLine(Offset(width / 1.7, height / 6),
+        Offset(width / 2.5, height / 6), _paint(Colors.red));
+
+    ///coming from top right point to back down to match tilt no1
+    ///this is tilt no2
+    canvas.drawLine(Offset(width / 6.5, height / 1.7),
+        Offset(width / 1.7, height / 6), _paint(Colors.green));
+
+    ///Line joining bottom of tilt one and two
+    canvas.drawLine(Offset(width / 6.5, height / 1.7),
+        Offset(width / 20, height / 2), _paint(Colors.blue));
+
+    ///parallel line to top most line (joins tilt no3's top and tilt no4's top)
+    canvas.drawLine(Offset(width / 2.5, height / 2),
+        Offset(width / 1.7, height / 2), _paint(Colors.blue));
+
+    ///tilt no 3
+    canvas.drawLine(Offset(width / 2.5, height / 2),
+        Offset(width / 4.4, height / 1.5), _paint(Colors.amber));
+
+    ///titl no 4
+    canvas.drawLine(Offset(width / 1.7, height / 2),
+        Offset(width / 3.1, height / 1.33), _paint(Colors.green));
+
+    ///bottom line joining title no3's bottom and tilt no4's bottom
+    canvas.drawLine(Offset(width / 4.4, height / 1.5),
+        Offset(width / 2.5, height / 1.2), _paint(Colors.red));
+
+    ///bottom most parallel line
+    canvas.drawLine(Offset(width / 1.7, height / 1.2),
+        Offset(width / 2.5, height / 1.2), _paint(Colors.amber));
+
+    ///middle line
+    canvas.drawLine(Offset(width / 1.7, height / 1.2),
+        Offset(width / 3.17, height / 1.72), _paint(Colors.blue));
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+
+  _paint(Color color) {
+    return Paint()
+      ..color = color
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 4.0;
   }
 }
