a8e88119a3a682d34ec24f7862d54c84d0a81614
Annsh Singh <annsh@so.city>
2019-04-10 12:45:30 +0530

    Updated Positioned widget

---
+++ lib/screens/positioned.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_guide/utils.dart';
+
+class PositionedWidget extends StatefulWidget {
+  @override
+  _PositionedWidgetState createState() => _PositionedWidgetState();
+}
+
+class _PositionedWidgetState extends State<PositionedWidget> {
+  var _topPosition = 20.0;
+  var _leftPosition = 20.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          child: Center(
+            child: Text(
+              'Positioned Widget',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Utils.ubuntuRegularFont),
+            ),
+          ),
+          margin: EdgeInsets.only(right: 48),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 12, right: 12, top: 12),
+                  height: 300,
+                  color: Colors.amber,
+                  width: MediaQuery.of(context).size.width,
+                ),
+                Positioned(
+                  top: _topPosition,
+                  left: _leftPosition,
+                  width: 50,
+                  height: 50,
+                  child: FlutterLogo(
+                    colors: Colors.lightBlue,
+                    textColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: Text(
+                'Drag the slider below to change the value of position of Flutter widget'
+                    ' from the top of the container',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 14.0,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: Utils.ubuntuRegularFont),
+              ),
+            ),
+            Container(
+              child: Slider(
+                value: _topPosition,
+                activeColor: Colors.lightBlue,
+                inactiveColor: Colors.lightBlue[50],
+                min: 20.0,
+                max: 250.0,
+                divisions: 100,
+                label: '${_topPosition.round()}',
+                onChanged: (double value) {
+                  setState(() {
+                    _topPosition = value;
+                  });
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              child: Text(
+                'Drag the slider below to change the value of position of Flutter widget'
+                    ' from the left of the container',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 14.0,
+                    fontStyle: FontStyle.italic,
+                    fontFamily: Utils.ubuntuRegularFont),
+              ),
+            ),
+            Container(
+              child: Slider(
+                value: _leftPosition,
+                activeColor: Colors.lightBlue,
+                inactiveColor: Colors.lightBlue[50],
+                min: 20.0,
+                max: (MediaQuery.of(context).size.width) - 60.0,
+                divisions: 100,
+                label: '${_leftPosition.round()}',
+                onChanged: (double value) {
+                  setState(() {
+                    _leftPosition = value;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
