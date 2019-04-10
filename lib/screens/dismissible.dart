8844ec9166d168eae383364fa1f26b80657030ec
Annsh Singh <annsh@so.city>
2019-04-10 12:46:22 +0530

    Updated Dismissible widget

---
+++ lib/screens/dismissible.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_widget_guide/utils.dart';
+
+class DismissibleWidget extends StatefulWidget {
+  @override
+  _DismissibleWidgetState createState() => _DismissibleWidgetState();
+}
+
+class _DismissibleWidgetState extends State<DismissibleWidget> {
+  final items = List<String>.generate(30, (i) => "Item ${i + 1}");
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          child: Center(
+            child: Text(
+              'Dismissible Widget',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Utils.ubuntuRegularFont),
+            ),
+          ),
+          margin: EdgeInsets.only(right: 48),
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+
+          return Dismissible(
+            // Each Dismissible must contain a Key. Keys allow Flutter to
+            // uniquely identify Widgets.
+            key: Key(item),
+            // We also need to provide a function that tells our app
+            // what to do after an item has been swiped away.
+            onDismissed: (direction) {
+              // Remove the item from our data source.
+              setState(() {
+                items.removeAt(index);
+              });
+
+              // Then show a snackbar!
+              Scaffold.of(context)
+                  .showSnackBar(SnackBar(content: Text("$item dismissed")));
+            },
+            // Show a green background as the item is swiped from left to right
+            background: Container(
+              padding: EdgeInsets.only(left: 12),
+              alignment: Alignment.centerLeft,
+              color: Colors.green,
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text(
+                      "Positive Action",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Icon(
+                    Icons.check,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            // Show a red background as the item is swiped from right to left
+            secondaryBackground: Container(
+              padding: EdgeInsets.only(right: 12),
+              alignment: Alignment.centerRight,
+              color: Colors.red,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Text(
+                      "Negative Action",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  Icon(
+                    Icons.cancel,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ),
+            child: ListTile(
+              leading: Icon(Icons.swap_horiz),
+              title: Text('$item'),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
