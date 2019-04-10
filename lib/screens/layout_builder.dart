5a05b4015aa287227e6616c47054256cceeeedc2
Annsh Singh <annsh@so.city>
2019-04-10 12:42:59 +0530

    Updated LayoutBuilder  widget

--- lib/screens/layout_builder.dart
+++ lib/screens/layout_builder.dart
@@ -1,8 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widget_guide/utils.dart';
 
 class LayoutBuilderWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Container(
+          child: Text(
+            'LayoutBuilder Widget',
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: Utils.ubuntuRegularFont),
+          ),
+        ),
+      ),
+      body: Center(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            if(constraints.maxWidth < 600 ){
+              return Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                    Expanded(
+                      child: Container(color: Colors.lightBlue),
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: Container(color: Colors.indigo),
+                      flex: 1,
+                    )
+                ],
+              );
+            }else{
+              return Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(color: Colors.lightBlue),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: Container(color: Colors.indigo),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: Container(color: Colors.pink),
+                    flex: 1,
+                  ),
+                  Expanded(
+                    child: Container(color: Colors.blue),
+                    flex: 1,
+                  )
+                ],
+              );
+            }
+          },
+        ),
+      ),
+    );
   }
 }
