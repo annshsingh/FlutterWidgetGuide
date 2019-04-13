c5a29fd09a20b32c55dc978b3a6277e80b8a90f3
Annsh Singh <annsh@so.city>
2019-04-14 00:29:24 +0530

    Updated the Animated Container Widget

--- lib/screens/animated_container.dart
+++ lib/screens/animated_container.dart
@@ -8,13 +8,13 @@ class AnimatedContainerWidget extends StatefulWidget {
 }
 
 class _AnimatedContainerWidgetState extends State<AnimatedContainerWidget> {
-  var _alignment = Alignment.center;
-  var _animatedHeight = 200.0;
-  var _animatedWidth = 400.0;
-  var _iconHeight = 180.0;
-  var _iconWidth = 180.0;
-  var _animatedColor = Colors.red;
-  var _isAnimated = false;
+  Alignment _alignment = Alignment.center;
+  double _animatedHeight = 200.0;
+  double _animatedWidth = 400.0;
+  double _iconHeight = 180.0;
+  double _iconWidth = 180.0;
+  Color _animatedColor = Colors.red;
+  bool _isAnimated = false;
 
   @override
   Widget build(BuildContext context) {
@@ -37,16 +37,22 @@ class _AnimatedContainerWidgetState extends State<AnimatedContainerWidget> {
         child: Container(
           margin: EdgeInsets.only(top: 24),
           child: Center(
+            ///Animated Container to change the background color with
+            ///an animation
             child: AnimatedContainer(
               duration: Duration(seconds: 2),
               width: _animatedWidth,
               height: _animatedHeight,
               color: _animatedColor,
               curve: Curves.fastOutSlowIn,
+              ///Animated container that changes the alignment of
+              ///FlutterLogo with an animation
               child: AnimatedContainer(
                 duration: Duration(seconds: 2),
                 curve: Curves.fastOutSlowIn,
                 alignment: _alignment,
+                ///Animated Container to change the dimentions of the
+                ///FlutterLogo with an animation
                 child: AnimatedContainer(
                   duration: Duration(seconds: 2),
                   curve: Curves.fastOutSlowIn,
@@ -66,20 +72,29 @@ class _AnimatedContainerWidgetState extends State<AnimatedContainerWidget> {
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Colors.green,
         onPressed: () => setState(() {
+              /// Condition to toggle the _isAnimated bool variable
               _animatedWidth == 400.0
                   ? _isAnimated = true
                   : _isAnimated = false;
+              /// Condition to change the alignment of Flutter logo
               _isAnimated
                   ? _alignment = Alignment.topCenter
                   : _alignment = Alignment.center;
+              /// Condition to change the width of the background container
               _isAnimated ? _animatedWidth = 200.0 : _animatedWidth = 400.0;
+              /// Condition to change the height of background container
+              /// MediaQuery.of(context).size.height - 200 is done to keep
+              /// height always above the FAB
               _isAnimated
                   ? _animatedHeight = MediaQuery.of(context).size.height - 200
                   : _animatedHeight = 200.0;
+              /// Condition to change the background color
               _isAnimated
                   ? _animatedColor = Colors.lightBlue
                   : _animatedColor = Colors.red;
+              /// Condition to change the width of FlutterLogo
               _isAnimated ? _iconWidth = 100.0 : _iconWidth = 180.0;
+              /// Condition to change the height of FlutterLogo
               _isAnimated ? _iconHeight = 100.0 : _iconHeight = 180.0;
             }),
         icon: Icon(
