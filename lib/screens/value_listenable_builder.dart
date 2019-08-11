7de9b8c32cd5447dfd0bfc3145a5f0d59d531e0d
Annsh Singh <annsh@so.city>
2019-08-11 16:01:13 +0530

    ValueListenableBuilder added

--- lib/screens/value_listenable_builder.dart
+++ lib/screens/value_listenable_builder.dart
@@ -3,10 +3,15 @@ import 'package:flutter_widget_guide/utils.dart';
 
 class ValueListenableBuilderWidget extends StatefulWidget {
   @override
-  _ValueListenableBuilderWidgetState createState() => _ValueListenableBuilderWidgetState();
+  _ValueListenableBuilderWidgetState createState() =>
+      _ValueListenableBuilderWidgetState();
 }
 
-class _ValueListenableBuilderWidgetState extends State<ValueListenableBuilderWidget> {
+class _ValueListenableBuilderWidgetState
+    extends State<ValueListenableBuilderWidget> {
+  /// This is the value you want your widgets to listen to.
+  final ValueNotifier<int> _counter = ValueNotifier<int>(2);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,20 +26,65 @@ class _ValueListenableBuilderWidgetState extends State<ValueListenableBuilderWid
         ),
       ),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Container(
-            color: Colors.white,
-            child: Text(
-              "This widget will be added soon",
-              style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: Utils.ubuntuRegularFont),
-            ),
+        child: ValueListenableBuilder(
+          builder: (BuildContext context, int value, Widget child) {
+            // This builder will only get called when the _counter
+            // is updated.
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    '$value',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 34.0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'The number is now ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.normal, fontSize: 18.0),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: value.remainder(2) == 0
+                        ? Text(
+                            "Even",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24.0),
+                          )
+                        : Text(
+                            "Odd",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 24.0),
+                          ),
+                  ),
+                ),
+                child,
+              ],
+            );
+          },
+          valueListenable: _counter,
+          // The child parameter is most helpful if the child is
+          // expensive to build and does not depend on the value from
+          // the notifier.
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text("I dont care about the value"),
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.plus_one),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        onPressed: () => _counter.value += 1,
       ),
     );
   }
