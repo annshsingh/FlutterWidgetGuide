a817f217da3694d1242c3b4a4e4bc62f00ed10b3
Annsh Singh <annsh@so.city>
2019-08-13 12:23:49 +0530

    Updated Spacer Widget

--- lib/screens/spacer.dart
+++ lib/screens/spacer.dart
@@ -7,6 +7,14 @@ class SpacerWidget extends StatefulWidget {
 }
 
 class _SpacerWidgetState extends State<SpacerWidget> {
+  bool isSpacerEnabled = true;
+
+  MaterialColor fabColor = Colors.red;
+
+  String fabText = "Remove Spacer";
+
+  IconData fabIcon = Icons.cancel;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,37 +29,73 @@ class _SpacerWidgetState extends State<SpacerWidget> {
         ),
       ),
       body: Center(
-          child: Row(
-        children: <Widget>[
-          /// Empty space with flex = 2
-          Spacer(
-            flex: 2,
-          ),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.green,
-          ),
-          /// Empty space with default flex = 1
-          Spacer(),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.blue,
-          ),
-          /// Empty space with default flex = 1
-          Spacer(),
-          Container(
-            height: 100,
-            width: 100,
-            color: Colors.orange,
-          ),
-          /// Empty space with default flex = 2
-          Spacer(
-            flex: 2,
-          ),
-        ],
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 18.0),
+              child: Text(
+                  "Empty spaces between the boxes below are Spacer widgets"),
+            ),
+            Row(
+              children: <Widget>[
+                /// Empty space with flex = 2
+                isSpacerEnabled
+                    ? Spacer(
+                        flex: 2,
+                      )
+                    : Container(),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.green,
+                ),
+
+                /// Empty space with default flex = 1
+                isSpacerEnabled ? Spacer() : Container(),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.blue,
+                ),
+
+                /// Empty space with default flex = 1
+                isSpacerEnabled ? Spacer() : Container(),
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.orange,
+                ),
+
+                /// Empty space with default flex = 2
+                isSpacerEnabled
+                    ? Spacer(
+                        flex: 2,
+                      )
+                    : Container(),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: fabColor,
+        onPressed: () => setState(() {
+          isSpacerEnabled == true ? isSpacerEnabled = false : isSpacerEnabled = true;
+          isSpacerEnabled ? fabColor = Colors.red : fabColor = Colors.green;
+          isSpacerEnabled ? fabIcon = Icons.cancel : fabIcon = Icons.add_circle;
+          isSpacerEnabled ? fabText = "Remove Spacer" : fabText = "Add Spacer";
+        }),
+        icon: Icon(
+          fabIcon,
+          color: Colors.white,
+        ),
+        label: Text(
+          fabText,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
     );
   }
 }
