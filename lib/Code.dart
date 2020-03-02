8fc93f118311b9215fd5688512ac772187babbb8
annshsingh <annsh29@gmail.com>
2020-03-02 19:25:09 +0530

    Added ListWheelScrollView widget

--- lib/Code.dart
+++ lib/Code.dart
@@ -8131,6 +8131,116 @@ class Code {
         );
       }
     }''';
-  static const String listWheelScrollViewWidgetCode = ''' ''';
+  static const String listWheelScrollViewWidgetCode = ''' 
+    class ListWheelScrollViewWidget extends StatefulWidget {
+      @override
+      _ListWheelScrollViewWidgetState createState() =>
+          _ListWheelScrollViewWidgetState();
+    }
+    
+    class _ListWheelScrollViewWidgetState extends State<ListWheelScrollViewWidget> {
+      ///Initial Slider widget value
+      var _value = 0.0;
+    
+      final List<Widget> entries = List<Widget>.generate(
+        15,
+        (i) => Card(
+          color: Utils.getRandomColor(),
+          child: Center(
+            child: Text(
+              "Item i + 1",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      );
+    
+      @override
+      void initState() {
+        //Hide banner ad if it isn't already hidden
+        Ads.hideBannerAd();
+        super.initState();
+      }
+    
+      @override
+      Widget build(BuildContext context) {
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'ListWheelScrollView Widget',
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: Utils.ubuntuRegularFont),
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.code),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        CodeScreen(code: Code.listWheelScrollViewWidgetCode),
+                  ),
+                ),
+              )
+            ],
+          ),
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Flexible(
+                flex: 8,
+                child: ListWheelScrollView(
+                  children: entries,
+                  itemExtent: 100,
+                  offAxisFraction: _value,
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Center(
+                  child: Text(
+                    "Drag to change offAxisFraction value",
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Slider(
+                  value: _value,
+    
+                  ///Color for active side of Slider
+                  activeColor: Colors.lightBlue,
+    
+                  ///Color for inactive side of Slider
+                  inactiveColor: Colors.lightBlue[50],
+    
+                  ///Minimum value of the slider
+                  min: -1.0,
+    
+                  ///Maximum value of the slider
+                  max: 1.0,
+    
+                  ///No.Of divisions from min to max value on the Slider
+                  divisions: 100,
+    
+                  ///Value indicator above the slider
+                  label: "_value.toStringAsFixed(2)",
+                  onChanged: (double value) {
+                    setState(
+                      () {
+                        _value = value;
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    }''';
   static const String shaderMaskWidgetCode = ''' ''';
 }
--- lib/screens/listwheelscrollview_widget.dart
+++ lib/screens/listwheelscrollview_widget.dart
@@ -7,10 +7,27 @@ import '../Ads.dart';
 
 class ListWheelScrollViewWidget extends StatefulWidget {
   @override
-  _ListWheelScrollViewWidgetState createState() => _ListWheelScrollViewWidgetState();
+  _ListWheelScrollViewWidgetState createState() =>
+      _ListWheelScrollViewWidgetState();
 }
 
 class _ListWheelScrollViewWidgetState extends State<ListWheelScrollViewWidget> {
+  ///Initial Slider widget value
+  var _value = 0.0;
+
+  final List<Widget> entries = List<Widget>.generate(
+    15,
+    (i) => Card(
+      color: Utils.getRandomColor(),
+      child: Center(
+        child: Text(
+          "Item ${i + 1}",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ),
+  );
+
   @override
   void initState() {
     //Hide banner ad if it isn't already hidden
@@ -36,15 +53,64 @@ class _ListWheelScrollViewWidgetState extends State<ListWheelScrollViewWidget> {
             onPressed: () => Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => CodeScreen(code: Code.listWheelScrollViewWidgetCode),
+                builder: (context) =>
+                    CodeScreen(code: Code.listWheelScrollViewWidgetCode),
               ),
             ),
           )
         ],
       ),
-      body: Container(
-        width: double.infinity,
-        child: Column(),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: <Widget>[
+          Flexible(
+            flex: 8,
+            child: ListWheelScrollView(
+              children: entries,
+              itemExtent: 100,
+              offAxisFraction: _value,
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Center(
+              child: Text(
+                "Drag to change offAxisFraction value",
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Slider(
+              value: _value,
+
+              ///Color for active side of Slider
+              activeColor: Colors.lightBlue,
+
+              ///Color for inactive side of Slider
+              inactiveColor: Colors.lightBlue[50],
+
+              ///Minimum value of the slider
+              min: -1.0,
+
+              ///Maximum value of the slider
+              max: 1.0,
+
+              ///No.Of divisions from min to max value on the Slider
+              divisions: 100,
+
+              ///Value indicator above the slider
+              label: "${_value.toStringAsFixed(2)}",
+              onChanged: (double value) {
+                setState(
+                  () {
+                    _value = value;
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
